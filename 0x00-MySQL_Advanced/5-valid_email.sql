670b33bea6d4398540a302a291cc155000795e17
g3rry9898 <hydrogenbond36@gmail.com>
2024-10-19 03:55:34 -0700

    l

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- Trigger to reset the valid_email attribute when the email has been changed
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;
+
